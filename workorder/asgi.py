42a2f53c6d6e550a019df841f7fb26b895f3e3d7
Pabz <64668341+ItzPabz@users.noreply.github.com>
2025-07-29 12:36:26 -0400

    Created Apps

--- workorder/asgi.py
+++ workorder/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for workorders project.
+ASGI config for workorder project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workorders.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workorder.settings')
 
 application = get_asgi_application()
